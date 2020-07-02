958608392fa33fe75935b147ac9827682afdd358
fabemiliano <fab.emiliano@gmail.com>
2020-07-02 17:59:41 -0300

    Updates

---
+++ precache-manifest.c176327949e43f752a688d3f3bcfbc6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e662dca5a16ab782e4561e6f1d715935",
+    "url": "/frontend-online-storels/index.html"
+  },
+  {
+    "revision": "93a272b048bfb6639d45",
+    "url": "/frontend-online-storels/static/css/main.ae25bb58.chunk.css"
+  },
+  {
+    "revision": "4595d6238f8621fa5851",
+    "url": "/frontend-online-storels/static/js/2.2ae8694d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/frontend-online-storels/static/js/2.2ae8694d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93a272b048bfb6639d45",
+    "url": "/frontend-online-storels/static/js/main.2e27120e.chunk.js"
+  },
+  {
+    "revision": "8899cfd53d66828b2823",
+    "url": "/frontend-online-storels/static/js/runtime-main.bd61a200.js"
+  },
+  {
+    "revision": "2ad5e133fda3490eda318fb5f384aefd",
+    "url": "/frontend-online-storels/static/media/carrinho.2ad5e133.png"
+  },
+  {
+    "revision": "c343ae9b9b4e0ad92e0b20b130e339ff",
+    "url": "/frontend-online-storels/static/media/cart.c343ae9b.png"
+  }
+]);
